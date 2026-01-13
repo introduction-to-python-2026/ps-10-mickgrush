71b24b5b53ddc200010db5c0b1b4ffe4ecf9aa3f
mickgrush <mick.grush@gmail.com>
2026-01-13 15:45:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from image_utils import load_image, edge_detection
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.filters import median
+from skimage.morphology import ball
+from PIL import Image
 
+
+image = load_image('/content/GettyImages-681946576-1024x716.jpg')
+clean_image = median(image, ball(3))
+edge_mag = edge_detection(clean_image)
+threshold = np.percentile(edge_mag, 90)
+edge_binary = edge_mag > threshold
+edge_image = Image.fromarray(edge_binary)
+edge_image.save('my_edges.png')
