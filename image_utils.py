33aa8d55dd07dd523ad2377b0401ab9e939aa5b1
mickgrush <mick.grush@gmail.com>
2026-01-13 15:47:47 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,9 +1,27 @@
 from PIL import Image
 import numpy as np
 from scipy.signal import convolve2d
+import matplotlib.pyplot as plt
+
 
 def load_image(path):
-    pass # Replace the `pass` with your code
+  image = Image.open(path)
+  image_arr = np.array(image)
+  return image_arr
 
 def edge_detection(image):
-    pass # Replace the `pass` with your code
+  #grayscale
+  grayscale_image = np.mean(image, axis=2)
+  #gray scale show: plt.imshow(grayscale_image, cmap = 'gray')
+
+
+  # edge finding
+  kernelx = np.array([[1,2,1],[0,0,0],[-1,-2,-1]])
+  kernely = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
+
+  edge_x = convolve2d(grayscale_image, kernelx, mode='same', boundary='fill', fillvalue=0)
+  edge_y = convolve2d(grayscale_image, kernely, mode='same', boundary='fill', fillvalue=0)
+  #edgeMAG
+  edgeMAG = (edge_x**2 + edge_y**2)**(1/2)
+
+  return edgeMAG
